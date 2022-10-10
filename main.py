e3f632565b8fdd917ffc44eceefea689e45766dc
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-10 23:35:34 +0000

    7-1

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 numbers = list(map(int, input().split()))
 sum = 0
 for i in range(len(numbers)):
-    sum += i
-average = sum/len(numbers)
+    sum += numbers[i]
+average = float(sum/len(numbers))
 dist = []
 for x in range(len(numbers)):
     if average > numbers[x]:
-        dist.append(int(average - numbers[x]))
+        dist.append(float(average - numbers[x]))
     else:
-        dist.append(int(numbers[x] - average))
+        dist.append(float(numbers[x] - average))
+    dist
 for j in range(len(numbers)):
     print(f'{dist[j]:.2f}', end=" ")
 # ******************************
